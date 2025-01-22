25d2410f248543318c0e360556f9764d912c6f8d
박민기 <minki20021@naver.com>
2025-01-22 17:01:33 +0900

    2025-01-22
    hi

---
+++ My project/Assets/Scripts/Add Component.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class AddComponent : MonoBehaviour
+{
+
+    [Header("플레이어의 이름")]
+    public string player_name;
+
+    [Header("클래스")]
+    public string player_class;
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# MyUnityRepository
- sdfdf
