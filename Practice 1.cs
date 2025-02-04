dc8e1ed0847f5fa7fdeb0deb17bf3d31eea0ec44
DreamsC0meTrue <minki20021@naver.com>
2025-02-04 09:29:22 +0900

    Update Practice 1.cs

--- Practice 1.cs
+++ Practice 1.cs
@@ -5,7 +5,7 @@ class Program
 {
     static void Main()
     {
-        Random random = new Random();
+        Random random = new Random(); //Copilot이 다함
         int[] randomNumbers = Enumerable.Range(1, 52).OrderBy(x => random.Next()).Take(8).ToArray();
         Console.WriteLine("Generated Random Numbers: " + string.Join(", ", randomNumbers));
     }
