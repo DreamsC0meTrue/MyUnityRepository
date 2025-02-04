677efb5df42dd99de488669afac4838d3b4b9675
DreamsC0meTrue <minki20021@naver.com>
2025-02-04 09:28:18 +0900

    Add files via upload

---
+++ Practice 1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int[] randomNumbers = Enumerable.Range(1, 52).OrderBy(x => random.Next()).Take(8).ToArray();
+        Console.WriteLine("Generated Random Numbers: " + string.Join(", ", randomNumbers));
+    }
+}
